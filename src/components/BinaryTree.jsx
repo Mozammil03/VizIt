afa806535163662699ca5c930da5fd896f1a4fa6
Mozammil <alammozammil03@gmail.com>
2024-11-04 19:44:06 +0530

    Updated BinaryTree

--- src/components/BinaryTree.jsx
+++ src/components/BinaryTree.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import NoUpload from '../utils/NoUpload'
 class Node{
   constructor() {
     this.x = x;
@@ -31,7 +32,7 @@ const BinaryTree = () => {
 
 
   return (
-    <div>gaved Up</div>
+    <NoUpload/>
   )
 }
 
